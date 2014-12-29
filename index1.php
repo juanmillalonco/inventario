9ef5e6ed771dfeb1c8b0658b904a72b145d7ef85
juanmillalonco <juanmillalonco88@gmail.com>
2014-12-29 15:18:25 -0300

    Formulario con mas campos

--- index1.php
+++ index1.php
@@ -2,8 +2,16 @@
 <html>
 <head>
 	<title>Inventario </title>
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
 </head>
 <body>
+<header>
+	<figure id="logo">
+    	<img src="img/logo.png" />
+    </figure>
+    	<h1>Inventario Aduana Puerto Aysen</h1>
+    	<nav></nav>
+</header>
 	<form>
 		<fieldset>
 			<p>Unidad</p>
@@ -21,21 +29,34 @@
 			<input type="text" name="descripcion" placeholder="Ingrese la descripcion" required />
 			<br/>
 			<p>Estado</p>
-			<input list="estado" name="estado" placeholder="Seleccione el estado" required>
+			<input list="estado" name="estado" placeholder="Seleccione el estado" required />
     		     <datalist id="estado">
     		        <option value="B">
     		        <option value="R">
     		        <option value="M">	
     		     </datalist>
 			<br />
+			<p>Usuario Asignado</p>
+			<input list="asignado" name="asignado" placeholder="Seleccione el usuario" required />
+    		     <datalist id="asignado">
+    		        <option value="Rodrigo Vera">
+    		        <option value="Sergio Leal">
+    		        <option value="Ivan Cardenas">	
+    		     </datalist>
+			<br />
 			<p>Nro de Serie</p>
-			<input type="text" name="serie" placeholder="ingrese el numero de serie" required/>
+			<input type="text" name="serie" placeholder="ingrese el numero de serie" />
 			<p>Observacion</p>
 			<textarea name="observacion" placeholder="¿Alguna observacion?">
 				
 			</textarea>
+			<br/>
+			<input type="submit" value="agregar" />
 		</fieldset>
-		<input type="submit" value="agregar" />
+		
 	</form>
+	 <footer>
+    	<strong>Administracion Aduana Puerto Aysen</strong>
+    </footer>
 </body>
 </html>
